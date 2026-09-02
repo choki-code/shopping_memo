5f04f27e16a0f9ba3808affef060107912f9c7cf
choki-code <923choki@gmail.com>
2026-09-02 11:08:56 +0900

    品目（Item）モデルと買い物リストとの関連を追加

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,3 +1,4 @@
 class ShoppingList < ApplicationRecord
   validates :name, presence: true
+  has_many :items, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_29_125522) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_02_015416) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.bigint "shopping_list_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shopping_list_id"], name: "index_items_on_shopping_list_id"
+  end
 
   create_table "shopping_lists", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,4 +28,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_29_125522) do
     t.string "name"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "items", "shopping_lists"
 end
