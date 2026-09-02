e0939eead344e4c11e5a0bcfcfc51244ab08a775
choki-code <923choki@gmail.com>
2026-09-02 17:49:44 +0900

    品目の追加・削除のsystem spec を追加

---
+++ spec/system/items_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe "品目", type: :system do
+  let(:shopping_list) do
+    ShoppingList.create!(name: "週末の買い物", memo: "牛乳とパンを買う")
+  end
+
+  describe "詳細画面から追加する" do
+    it "追加した品目が一覧に表示される" do
+      visit shopping_list_path(shopping_list)
+
+      fill_in "Name", with: "キャベツ"
+      click_on "Create Item"
+
+      expect(page).to have_content "Item was successfully created."
+      expect(page).to have_content "キャベツ"
+    end
+
+    it "名前が空のときは追加できず、エラーが表示される" do
+      visit shopping_list_path(shopping_list)
+
+      click_on "Create Item"
+
+      expect(page).to have_content "Name can't be blank"
+      expect(shopping_list.items.count).to eq 0
+    end
+  end
+
+  describe "詳細画面から削除する" do
+    it "削除した品目が一覧から消える" do
+      shopping_list.items.create!(name: "キャベツ")
+
+      visit shopping_list_path(shopping_list)
+      click_on "Destroy this item"
+
+      expect(page).to have_content "Item was successfully destroyed."
+      expect(page).to have_no_content "キャベツ"
+    end
+  end
+
+  describe "買い物リストごと削除する" do
+    it "リストを削除すると品目も消える" do
+      shopping_list.items.create!(name: "キャベツ")
+
+      visit shopping_list_path(shopping_list)
+      click_on "Destroy this shopping list"
+
+      expect(page).to have_content "Shopping list was successfully destroyed."
+      expect(Item.count).to eq 0
+    end
+  end
+end
