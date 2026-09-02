9824e4eb84e29e2ab271fbce3faf92a6f6e0cc86
choki-code <923choki@gmail.com>
2026-09-02 14:46:06 +0900

    品目の追加・削除のルーティングとコントローラを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :shopping_lists
+  resources :shopping_lists do
+    resources :items, only: %i[ create destroy ]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
